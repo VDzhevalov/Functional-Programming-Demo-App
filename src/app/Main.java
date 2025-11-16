189cb0a8fb4b8013ec85718a28bd04b2ef4595ed
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-11-16 21:31:56 +0200

    Add implementation MathOperation using Anonymous class and method reference.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,24 @@
 package app;
 
+import app.model.MathOperation;
+
+import static app.view.UIOperator.getOutput;
+
 public class Main {
 
     public static void main(String[] args) {
+        //1. Створіть функціональний інтерфейс MathOperation, який має один метод int operate(int a, int b).
+        //2. Використовуючи анонімний клас, створіть об'єкт інтерфейсу MathOperation, який виконує операцію додавання.
+        MathOperation mathOperation = new MathOperation() {
+            @Override
+            public int operate(int a, int b){
+                return a + b;
+            }
+        };
+        getOutput("Анонімний клас MathOperation", mathOperation.operate(10, 20));
 
+        //2.1 Те ж саме, але використовуючи посилання на метод
+        MathOperation mathOperationMethodRef = Integer::sum;
+        getOutput("MathOperation Посилання на статичний метод = Integer::sum", mathOperationMethodRef.operate(15, 40) );
     }
 }
