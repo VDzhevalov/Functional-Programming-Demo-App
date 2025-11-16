d92929ab2799b855a12602a0c55b9295cece5bf2
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-11-16 21:53:57 +0200

    Reformat comments.

--- src/app/Main.java
+++ src/app/Main.java
@@ -13,8 +13,8 @@ import static app.view.UIOperator.getOutput;
 public class Main {
 
     public static void main(String[] args) {
-        //1. Створіть функціональний інтерфейс MathOperation, який має один метод int operate(int a, int b).
-        //2. Використовуючи анонімний клас, створіть об'єкт інтерфейсу MathOperation, який виконує операцію додавання.
+        /*      1. Створіть функціональний інтерфейс MathOperation, який має один метод int operate(int a, int b).
+                2. Використовуючи анонімний клас, створіть об'єкт інтерфейсу MathOperation, який виконує операцію додавання.*/
         MathOperation mathOperation = new MathOperation() {
             @Override
             public int operate(int a, int b) {
@@ -22,26 +22,30 @@ public class Main {
             }
         };
         getOutput("Анонімний клас MathOperation", mathOperation.operate(10, 20));
-        //2.1 Те ж саме, але використовуючи посилання на метод
+        /*      2.1 Те ж саме, але використовуючи посилання на метод.*/
         MathOperation mathOperationMethodRef = Integer::sum;
         getOutput("MathOperation Посилання на статичний метод = Integer::sum", mathOperationMethodRef.operate(15, 40));
 
-        //3. Створіть лямбда-вираз, який реалізує функціональний інтерфейс StringManipulator і перетворює вхідний рядок у верхній регістр
+        /*      3. Створіть лямбда-вираз, який реалізує функціональний інтерфейс StringManipulator і перетворює вхідний
+                    рядок у верхній регістр.*/
         StringManipulator stringManipulator = s -> s.toUpperCase();
         getOutput("Лямбда-вираз s -> s.toUpperCase()", stringManipulator.transform("Hello"));
-        //3.1 Те ж саме, але використовуючи посилання на метод
+        /*      3.1 Те ж саме, але використовуючи посилання на метод.*/
         StringManipulator stringManipulatorMethodRef = String::toUpperCase;
         getOutput("Посилання на статичний метод String::toUpperCase", stringManipulatorMethodRef.transform("Hello"));
 
-        //4. Створіть клас StringListProcessor, який має статичний метод int countUppercase(String s), що повертає кількість великих літер у рядку.
-        //5. Використайте посилання на методи, щоб передати метод countUppercase як аргумент функції Function<String, Integer>.
+        /*      4. Створіть клас StringListProcessor, який має статичний метод int countUppercase(String s), що повертає
+                    кількість великих літер у рядку.
+                5. Використайте посилання на методи, щоб передати метод countUppercase як аргумент функції Function<String, Integer>.*/
         Function<String, Integer> functionResult = StringListProcessor::countUppercase;
         getOutput("Посилання на статичний метод StringListProcessor::countUppercase", functionResult.apply("HEllO wORld"));
 
-        //6. Створіть клас RandomNumberGenerator, який має статичний метод int generateRandomNumber(int min, int max), що повертає випадкове ціле число в заданому діапазоні.
+        /*      6. Створіть клас RandomNumberGenerator, який має статичний метод int generateRandomNumber(int min, int max),
+                    що повертає випадкове ціле число в заданому діапазоні.*/
         getOutput("Статичний метод int generateRandomNumber(5,9)", generateRandomNumber(5, 9));
 
-        //7. Використайте інтерфейс Supplier<T> для створення постачальника, який буде генерувати випадкове число від 1 до 100.
+        /*      7. Використайте інтерфейс Supplier<T> для створення постачальника, який буде генерувати випадкове число
+                    від 1 до 100.*/
         Supplier<Integer> randomSupplier = () -> generateRandomNumber(1, 100);
         getOutput("Supplier () -> generateRandomNumber(1,100)", randomSupplier.get());
     }
