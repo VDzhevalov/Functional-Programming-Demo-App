dc46c5010b315dbe238a4a306bff51fdb38cbcb7
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-11-16 21:22:33 +0200

    Add UIOperator to output data.

---
+++ src/app/view/UIOperator.java
@@ -0,0 +1,13 @@
+package app.view;
+
+import java.util.function.Consumer;
+
+import static java.lang.String.format;
+
+public class UIOperator {
+    private static final Consumer<String> PRINTER = System.out::println;
+
+    public static<T> void getOutput(String description,T output) {
+        PRINTER.accept(format("%s: %s", description, output.toString()));
+    }
+}
